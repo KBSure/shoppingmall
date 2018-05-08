362f39786ef91098225fc40b180b1a02fa70a796
serena <noriming2@gmail.com>
2018-05-09 05:07:12 +0900

    feat .invalidateHttpSession(true)

--- src/main/java/com/project/shoppingmall/config/WebApplicationSecurity.java
+++ src/main/java/com/project/shoppingmall/config/WebApplicationSecurity.java
@@ -26,13 +26,15 @@ public class WebApplicationSecurity  extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/boards").and()
-                .authorizeRequests()
-                .antMatchers("/members/**").permitAll() //일단 DB에 SELECT로 가져와서 권한 검사는 함..
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true)
+                .and().authorizeRequests()
+                .antMatchers("/members/**").permitAll()
                 .antMatchers("/products/**").permitAll()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .and().formLogin()
-                .loginPage("/members/signin").usernameParameter("email").passwordParameter("password")//이게 없으면 스프링 기본 폼 사용됨
+                .loginPage("/members/signin").usernameParameter("email").passwordParameter("password")
                 .failureUrl("/main/error")
                 .defaultSuccessUrl("/", true)
                 .and().logout().permitAll();
