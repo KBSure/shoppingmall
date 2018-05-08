22fa1346641ae964fabdf0644b8d00f564962e0d
DESKTOP-AH83DAJ\Serena-Laptop <noriming2@gmail.com>
2018-05-08 11:49:08 +0900

    fix repository

--- src/main/java/com/project/shoppingmall/repository/OrdersRepository.java
+++ src/main/java/com/project/shoppingmall/repository/OrdersRepository.java
@@ -1,6 +1,7 @@
 package com.project.shoppingmall.repository;
 
 import com.project.shoppingmall.base.JpaQueryDslPredicateRepository;
+import com.project.shoppingmall.domain.Order;
 
-public interface OrdersRepository extends JpaQueryDslPredicateRepository {
+public interface OrdersRepository extends JpaQueryDslPredicateRepository<Order,Long>{
 }
--- src/main/java/com/project/shoppingmall/repository/PersistenceLoginsRepository.java
+++ src/main/java/com/project/shoppingmall/repository/PersistenceLoginsRepository.java
@@ -1,6 +1,7 @@
 package com.project.shoppingmall.repository;
 
 import com.project.shoppingmall.base.JpaQueryDslPredicateRepository;
+import com.project.shoppingmall.domain.PersistenceLogin;
 
-public interface PersistenceLoginsRepository extends JpaQueryDslPredicateRepository {
+public interface PersistenceLoginsRepository extends JpaQueryDslPredicateRepository<PersistenceLogin,String> {
 }
