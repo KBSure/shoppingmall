01ab9b1a824d2d3defc925c3574e99e4212ee92d
serena <noriming2@gmail.com>
2018-05-09 05:05:41 +0900

    fix MemberController스토리작성

--- src/main/java/com/project/shoppingmall/controller/MembersController.java
+++ src/main/java/com/project/shoppingmall/controller/MembersController.java
@@ -9,16 +9,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequestMapping("/members")
 public class MembersController {
-	
+
 	@Autowired
 	private MembersService membersService;
-	
+
 	@GetMapping("/signin")
 	public String signinForm() {
 		return "members/signin";
@@ -33,31 +31,39 @@ public class MembersController {
 	public String join(@ModelAttribute Member member){
 		//유효성 검사
 			//if(유효성 검사에 에러가 있음)
-				//return "members/join"
-			//if(패스워드 != 패스워드)
-				//bindingResult.error
+				//return join화면
+			//if(패스워드 != 패스워드확인)
+				//bindingResult.addError
+				//return join화면
+			//Repository에서 Email로 유저정보 받아옴
+			//유저정보가 있다면
+				//bindingResult.addError
+				//return join화면
 
 
 		PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 		member.setPasswd(passwordEncoder.encode(member.getPasswd()));
 
-		List<Role> roleList = new ArrayList<>();
-		Role userRole = new Role();
-		userRole.setName("USER");
-		roleList.add(userRole);
-		member.setRoles(roleList);
 
+		member.addRole(new Role().setRole("USER"));
 		Member saveMember = membersService.addMembers(member);
 
 		return "redirect:/main/main";
 	}
 
-	@GetMapping(path="/findid") public String findIdForm() {
+	@GetMapping(path="/findid")
+	public String findIdForm() {
 		return "members/find_id";
 	}
 
 	@PostMapping(path="/findid")
 	public String findId() {
+		//form에서 사용자이름과 전화번호 정보 받아오기
+		//List<Member> = Ropository에서 사용자 이름과 전화번호로 ID 받아오기
+		//사용자가 있음
+			//.attribute(첫번째)
+		//사용자가 없음
+			//bindingResult.addError
 		return "members/find_result";
 	}
 
@@ -68,16 +74,31 @@ public class MembersController {
 
 	@PostMapping(path="/findpassword")
 	public String findPasswd() {
+		//form에서 이메일과 전화번호 정보 받아오기
+		//List<Member> = Ropository에서 사용자 이름과 전화번호로 password 받아오기
+		//if(사용자가 있음)
+			//.attribute(첫번째)
+		//else(사용자가 없음)
+			//bindingResult.addError
 		return "members/find_result";
 	}
 
 	@GetMapping(path="/{id}/update")
 	public String updateAccountForm(@PathVariable("id") long id) {
+		//주문자ID,페이지정보,배송정보,날짜를 파라미터로 주문목록 리스트 가져오기
+		//페이져 초기화
+			//
+		//.attribute(페이지리스트)
+		//.attribute(페이져)
 		return "members/update_member";
 	}
 	@PutMapping(path="/{id}")
 	public String updateAccount()
 	{
+		//form에서 멤버 정보 받아오기
+		//int = setFixed한 결과값 받아오기
+		//if(결과 == update실패)
+			//bindingResult.addError
 		return "members/update_member";
 	}
 
@@ -91,8 +112,16 @@ public class MembersController {
 		return "members/dropout";
 	}
 
-	@DeleteMapping(path="/{id}") //메인으로 리다이렉트..
+	@DeleteMapping(path="/{id}")
 	public String dropOut(@PathVariable("id") long id) {
+		//form에서 password가져오기
+		//Repository에서 Email로 Password 받아옴
+		//if(패스워드 != 쿼리 패스워드)
+			//bindingResult.addError
+			//return dropout
+		//int = delete한 결과값 받아오기
+		//if(결과 == delete실패)
+			//bindingResult.addError
 		return "members";
 	}
 
@@ -103,6 +132,13 @@ public class MembersController {
 
 	@PutMapping(path="/{id}/password")
 	public String updatePasswd(@PathVariable("id") long id) {
+		//form에서 password가져오기
+		//if(패스워드 != 확인 패스워드)
+			//bindingResult.addError
+			//return update_password
+		//int = setFixed한 결과값 받아오기
+		//if(결과 == update실패)
+			//bindingResult.addError
 		 return "members/update_password";
 	}
 
