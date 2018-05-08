2fbcd98bfd30fef7e462caff4d66a5c73f0f8361
KBSure <kbsure.dev@gmail.com>
2018-05-08 11:34:36 +0900

    Cart, Image 레파지토리 생성

---
+++ src/main/java/com/project/shoppingmall/repository/CartRepository.java
@@ -0,0 +1,8 @@
+package com.project.shoppingmall.repository;
+
+import com.project.shoppingmall.base.JpaQueryDslPredicateRepository;
+import com.project.shoppingmall.domain.Cart;
+
+public interface CartRepository extends JpaQueryDslPredicateRepository<Cart, Long> {
+
+}
