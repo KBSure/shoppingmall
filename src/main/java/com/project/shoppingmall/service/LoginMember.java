398e65db731953357d2b7a64b920ee724b500265
DESKTOP-AH83DAJ\Serena-Laptop <noriming2@gmail.com>
2018-05-11 10:15:39 +0900

    add LoginMember

--- src/main/java/com/project/shoppingmall/service/LoginMember.java
+++ src/main/java/com/project/shoppingmall/service/LoginMember.java
@@ -1,4 +1,34 @@
 package com.project.shoppingmall.service;
 
-public class LoginMember {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+public class LoginMember extends User {
+    Long Id;
+    String name;
+    public LoginMember(String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, authorities);
+    }
+
+    public LoginMember(String username, String password, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
