3dc8cce57b4305d99a2cca861167c9f0325e9bac
DESKTOP-AH83DAJ\Serena-Laptop <noriming2@gmail.com>
2018-05-11 14:32:13 +0900

    add SinginParam

--- src/main/java/com/project/shoppingmall/dto/SigninPram.java
+++ src/main/java/com/project/shoppingmall/dto/SigninPram.java
@@ -1,4 +1,15 @@
 package com.project.shoppingmall.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class SigninPram {
+    long id;
+    String name;
+
+    public SigninPram(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
