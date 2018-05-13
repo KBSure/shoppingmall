b731b7cf9db941e401f287496186d4a37ca75c50
DESKTOP-AH83DAJ\Serena-Laptop <noriming2@gmail.com>
2018-05-14 00:33:27 +0900

    style 필요없는 기능 삭제

--- src/main/java/com/project/shoppingmall/config/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/project/shoppingmall/config/CustomAuthenticationSuccessHandler.java
@@ -1,28 +1,18 @@
 package com.project.shoppingmall.config;
 
-import com.project.shoppingmall.dto.SigninParam;
-import com.project.shoppingmall.security.LoginMember;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
 public class CustomAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
-        LoginMember loginMember = (LoginMember)authentication.getPrincipal();//DTO
-        request.setAttribute("signinParam",new SigninParam(loginMember.getId().toString(),loginMember.getName()));
-        request.getRequestDispatcher("/").forward(request,response);
 
-       // request.setAttribute("memberId",loginMember.getId());
-      //  request.setAttribute("name",loginMember.getName());
-
-        //TODO
-        //Referer처리하기
         super.onAuthenticationSuccess(request, response, authentication);
 
     }
