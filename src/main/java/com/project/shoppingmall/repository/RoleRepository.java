42aa9bbf7d7a8d2e760b4c543a9743ac808ef4e2
DESKTOP-AH83DAJ\Serena-Laptop <noriming2@gmail.com>
2018-05-18 11:28:51 +0900

    add roleRepository

--- src/main/java/com/project/shoppingmall/repository/RoleRepository.java
+++ src/main/java/com/project/shoppingmall/repository/RoleRepository.java
@@ -1,4 +1,9 @@
 package com.project.shoppingmall.repository;
 
-public class RoleRepository {
+import com.project.shoppingmall.base.JpaQueryDslPredicateRepository;
+import com.project.shoppingmall.domain.Role;
+import com.project.shoppingmall.domain.RoleName;
+//진행중
+public interface RoleRepository extends JpaQueryDslPredicateRepository<Role,Long>  {
+    Role findRoleByName(RoleName name);
 }
